8209696c078e28b3e3b302f0ec464cef310f7217
= <maheshkadambala18@gmail.com>
2024-12-07 17:49:33 +0530

    add routing

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <div className="header-contents">
+            <h2>Order your taste food here</h2>
+            <p>Choose the following order what you like</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
