9950fd184008308cbe2da074c1a8b38bba9ef28f
= <maheshkadambala18@gmail.com>
2024-12-14 18:23:09 +0530

    add search bar

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -27,7 +27,7 @@ const LoginPopup = ({ setShowLogin }) => {
                 <p>Create a new account? <span onClick={()=>setCurrentState("Sign Up")}>Click here</span></p>:
                 <p>Already have an account <span onClick={()=>setCurrentState("Login")}>Click here</span></p>}
                 
-                
+
             </form>
 
         </div>
