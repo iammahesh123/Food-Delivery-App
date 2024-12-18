e774df8edb2ecfc9b30e8451b1890f29fbfe9f03
= <maheshkadambala18@gmail.com>
2024-12-18 08:19:57 +0530

    add MenuHeader component

--- frontend/src/pages/MenuPage/MenuPage.jsx
+++ frontend/src/pages/MenuPage/MenuPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import MenuHeader from '../../components/MenuHeader/MenuHeader'
 
 const MenuPage = () => {
   return (
     <div>
-        <ExploreMenu/>
-        <FoodDisplay />  
+      <MenuHeader />
+      <ExploreMenu />
+      <FoodDisplay />
     </div>
   )
 }
